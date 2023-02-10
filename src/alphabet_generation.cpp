d7c21165cff4c8daf07b7eb9bdb5eb42d9c8b10f
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-11 01:41:32 +0700

    Removed_comments_in_alphabet_generation_cpp

---
+++ src/alphabet_generation.cpp
@@ -0,0 +1,41 @@
+#include "alphabet_generation.h"
+
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+const string digit_alphabet = "0123456789";
+const string small_character = "abcdefghijklmnopqrstuvwxyz";
+const string large_character = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string special_symbol = "!@#$^&*?";
+
+int generation_alphabet(
+        string* alphabet,
+        int* length_alphabet,
+        bool take_digit_flag,
+        bool take_small_symbol_flag,
+        bool take_large_symbol_flag,
+        bool take_special_symbol_flag)
+{
+    if (take_digit_flag) {
+        *alphabet += digit_alphabet;
+    }
+    if (take_small_symbol_flag) {
+        *alphabet += small_character;
+    }
+    if (take_large_symbol_flag) {
+        *alphabet += large_character;
+    }
+    if (take_special_symbol_flag) {
+        *alphabet += special_symbol;
+    }
+
+    *length_alphabet = alphabet->length();
+
+    if (*alphabet == "") {
+        *length_alphabet = 0;
+        return 1;
+    }
+    return 0;
+}
