2555a71903c796ea80ecb9f1ea759e63dbf53ff2
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-12 01:09:08 +0700

    gitignore

---
+++ test/tests.cpp
@@ -0,0 +1,17 @@
+#include "ctest.h"
+#include <string>
+#include "alphabet_generation.hpp"
+
+CTEST(ALPHABET_GENERATION, ALPHABET_FLAG)
+{
+    string alphabet = "";
+    int len_alphabet = 0;
+
+    ASSERT_EQUAL(1, generation_alphabet(&alphabet, &len_alphabet, false, false, false, false));
+
+    alphabet = "";
+    len_alphabet = 0;
+
+    ASSERT_EQUAL(0, generation_alphabet(&alphabet, &len_alphabet, true, true, true, true));
+    ASSERT_EQUAL(len_alphabet, 70);
+}
