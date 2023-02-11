763f9760a895a305eb7f5e19e456cd1d78b3b44d
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-11 22:58:18 +0700

    Wrote_tests_to_check_length_alphabet_return

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,4 +9,10 @@ CTEST(ALPHABET_GENERATION, ALPHABET_FLAG)
     int len_alphabet = 0;
 
     ASSERT_EQUAL(1, generation_alphabet(&alphabet, &len_alphabet, false, false, false, false));
+
+    alphabet = "";
+    len_alphabet = 0;
+
+    ASSERT_EQUAL(0, generation_alphabet(&alphabet, &len_alphabet, true, true, true, true));
+    ASSERT_EQUAL(len_alphabet, 70);
 }
