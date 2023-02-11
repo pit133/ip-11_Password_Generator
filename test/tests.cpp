3ac4a5af2af43b8ec4becf1b3f8fe176bba6da91
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-11 22:51:32 +0700

    Fix_start_test

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,5 +8,5 @@ CTEST(ALPHABET_GENERATION, ALPHABET_FLAG)
     string alphabet = "";
     int len_alphabet = 0;
 
-    ASSERT_EQUAL(generation_alphabet(&alphabet, &len_alphabet, false, false, false, false),0);
+    ASSERT_EQUAL(1, generation_alphabet(&alphabet, &len_alphabet, false, false, false, false));
 }
