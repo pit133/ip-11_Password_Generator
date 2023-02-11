f8a64f1d3df078c8918c4217bdc346ef98b3394f
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-02-11 19:39:54 +0700

    Create_first_test_alphabet

--- test/tests.c
+++ test/tests.c
@@ -1,8 +1,12 @@
 #include "ctest.h"
+#include <string>
 
 #include "alphabet_generation.h"
 
 CTEST(ALPHABET_GENERATION, ALPHABET_FLAG)
 {
+    string alphabet = "";
+    int len_alphabet = 0;
 
+    ASSERT_EQUAL(generation_alphabet(alphabet, len_alphabet, false, false, false, false),1);
 }
